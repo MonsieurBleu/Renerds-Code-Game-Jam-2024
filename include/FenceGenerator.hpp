59133f7ac09fff5e7d041a8d56b39d201c7397b6
ClairDeLuna01 <luna.bossu@gmail.com>
2024-01-23 21:36:47 +0100

    increased fence size

--- include/FenceGenerator.hpp
+++ include/FenceGenerator.hpp
@@ -1,6 +1,6 @@
 #include "Scene.hpp"
 #include "../Engine/include/PhysicsEngine.hpp"
 
-#define FENCE_SIZE 256
+#define FENCE_SIZE 512
 
 void generateFence(ModelRef fence, Scene &scene, PhysicsEngine &physicsEngine);
